5b3face9be6ed13d165b7dee5d35e432192e3c2b
Lalith143kl <lalithnarayanavuppala@gmail.com>
2023-04-19 18:37:47 -0500

    Done

--- controllers/pen.js
+++ controllers/pen.js
@@ -1,25 +1,33 @@
 var pen = require('../models/pen');
 // List of all pens
-exports.pen_list = async function(req, res) {
-    try{
+exports.pen_list = async function (req, res) {
+    try {
         thepen = await pen.find();
         res.send(thepen);
-        }
-        catch(err){
+    }
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
-        }
-//res.send('NOT IMPLEMENTED: pen list');
+    }
+    //res.send('NOT IMPLEMENTED: pen list');
 };
-// for a specific pen.
-exports.pen_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: pen detail: ' + req.params.id);
+//for a specific pen.
+exports.pen_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await pen.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
 // Handle pen create on POST.
 
 //res.send('NOT IMPLEMENTED: pen create POST');
 // Handle pen create on POST.
-exports.pen_create_post = async function(req, res) {
+exports.pen_create_post = async function (req, res) {
     console.log(req.body)
     let document = new pen();
     // We are looking for a body, since POST does not have query parameters.
@@ -29,36 +37,116 @@ exports.pen_create_post = async function(req, res) {
     document.Pen_color = req.body.Pen_color;
     document.Pen_Company = req.body.Pen_Company;
     document.Pen_cost = req.body.Pen_cost;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-   };
+};
 
 // Handle pen delete form on DELETE.
-exports.pen_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: pen delete DELETE ' + req.params.id);
-};
+// Handle pen delete on DELETE.
+exports.pen_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await pen.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
 // Handle pen update form on PUT.
-exports.pen_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: pen update PUT' + req.params.id);
+exports.pen_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await pen.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Pen_color)
+            toUpdate.Pen_color = req.body.Pen_color;
+        if (req.body.Pen_Company) toUpdate.Pen_Company = req.body.Pen_Company;
+        if (req.body.Pen_cost) toUpdate.Pen_cost = req.body.Pen_cost;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
 
 // VIEWS
 // Handle a show all view
-exports.pen_view_all_Page = async function(req, res) {
+exports.pen_view_all_Page = async function (req, res) {
+    try {
+        thepens = await pen.find();
+        res.render('pen', { title: 'pen Search Results', results: thepens });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.pen_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
     try{
-    thepens = await pen.find();
-    res.render('pen', { title: 'pen Search Results', results: thepens });
+    result = await pen.findById( req.query.id)
+    res.render('pendetail',
+   { title: 'pen Detail', toShow: result });
     }
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
 
+   
+   // Handle building the view for creating a pen.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.pen_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('pencreate', { title: 'pen Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
 
+};
+
+// Handle building the view for updating a pen.
+// query provides the id
+exports.pen_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await pen.findById(req.query.id)
+    res.render('penupdate', { title: 'pen Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle a delete one view with id from query
+exports.pen_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await pen.findById(req.query.id)
+    res.render('pendelete', { title: 'pen Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/pen.js
+++ routes/pen.js
@@ -1,6 +1,18 @@
 var express = require('express');
 const pen_controlers= require('../controllers/pen');
 var router = express.Router();
-/* GET costumes */
+/* GET pens */
 router.get('/', pen_controlers.pen_view_all_Page );
+
+/* GET detail pen page */
+router.get('/detail', pen_controlers.pen_view_one_Page);
+/* GET create pen page */
+router.get('/create', pen_controlers.pen_create_Page);
+
+
+/* GET create update page */
+router.get('/update', pen_controlers.pen_update_Page);
+
+/* GET delete Pen page */
+router.get('/delete', pen_controlers.pen_delete_Page);
 module.exports = router;
