3f38ab443f0db99a97a0cb2ee2a7838e60aa495b
Lalith143kl <lalithnarayanavuppala@gmail.com>
2023-04-22 12:13:10 -0500

    d

--- app.js
+++ app.js
@@ -8,30 +8,30 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username }, function (err, user) {
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
-  });
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
   }));
 
 require('dotenv').config();
 const connectionString =
-process.env.MONGO_CON
+  process.env.MONGO_CON
 mongoose = require('mongoose');
-mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){console.log("Connection to DB succeded")});
+db.once("open", function () { console.log("Connection to DB succeded") });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -57,9 +57,9 @@ app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
- }));
- app.use(passport.initialize());
- app.use(passport.session());
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -72,17 +72,17 @@ app.use('/resource', resourceRouter);
 // passport config
 // Use the existing connection
 // The Account model
-var Account =require('./models/account');
+var Account = require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -93,41 +93,41 @@ app.use(function(err, req, res, next) {
 });
 // We can seed the collection if needed on
 //server start
-async function recreateDB(){
-// Delete everything
-await pen.deleteMany();
-let instance1 = new pen({Pen_color:"pink", Pen_Company:'large',Pen_cost:25.4});
-instance1.save().then( () =>{
-console.log("First object saved");
-}).catch ((e)  =>{
-  console.log('There was an error',e.message);
-});
+async function recreateDB() {
+  // Delete everything
+  await pen.deleteMany();
+  let instance1 = new pen({ Pen_color: "pink", Pen_Company: 'large', Pen_cost: 25.4 });
+  instance1.save().then(() => {
+    console.log("First object saved");
+  }).catch((e) => {
+    console.log('There was an error', e.message);
+  });
 
-let instance2 = new pen({Pen_color:"Black",Pen_Company:"Butterfly",Pen_cost:30});
-instance2.save().then( () =>{
-console.log("Second object saved");
-}).catch ((e)  =>{
-  console.log('There was an error',e.message);
-});
-let instance3 = new pen({Pen_color:"Blue",Pen_Company:"NATRAJ",Pen_cost:20});
-instance3.save().then( () =>{
-console.log("Third object saved");
-}).catch ((e)  =>{
-  console.log('There was an error',e.message);
-});
+  let instance2 = new pen({ Pen_color: "Black", Pen_Company: "Butterfly", Pen_cost: 30 });
+  instance2.save().then(() => {
+    console.log("Second object saved");
+  }).catch((e) => {
+    console.log('There was an error', e.message);
+  });
+  let instance3 = new pen({ Pen_color: "Blue", Pen_Company: "NATRAJ", Pen_cost: 20 });
+  instance3.save().then(() => {
+    console.log("Third object saved");
+  }).catch((e) => {
+    console.log('There was an error', e.message);
+  });
 
 }
 let reseed = true;
-if (reseed) { recreateDB();}
+if (reseed) { recreateDB(); }
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -136,5 +136,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
- 
+
 module.exports = app;
