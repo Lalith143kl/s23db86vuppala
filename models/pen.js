bb235c80f9db6396bd270a80e7b322655393a6ad
Lalith143kl <lalithnarayanavuppala@gmail.com>
2023-04-27 21:56:59 -0500

    updated

--- models/pen.js
+++ models/pen.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose")
 const PenSchema = mongoose.Schema({
-    Pen_color: String,
+    
+    Pen_color:{
+        type:String,
+        minLength:2
+    },
     Pen_Company: String,
-    Pen_cost: Number
-})
+    Pen_cost:{
+        type: Number,
+    min:0,
+    max: 1000,
+   }, 
+});
 module.exports = mongoose.model("Pen",PenSchema)
--- routes/pen.js
+++ routes/pen.js
@@ -1,18 +1,29 @@
 var express = require('express');
 const pen_controlers= require('../controllers/pen');
 var router = express.Router();
-/* GET pens */
-router.get('/', pen_controlers.pen_view_all_Page );
+
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+
+/* GET home page. */
+router.get('/',pen_controlers.pen_view_all_Page);
 
 /* GET detail pen page */
 router.get('/detail', pen_controlers.pen_view_one_Page);
-/* GET create pen page */
-router.get('/create', pen_controlers.pen_create_Page);
 
+/* GET create pen page */
+router.get('/create',secured, pen_controlers.pen_create_Page);
 
 /* GET create update page */
-router.get('/update', pen_controlers.pen_update_Page);
+router.get('/update', secured,  pen_controlers.pen_update_Page);
 
-/* GET delete Pen page */
-router.get('/delete', pen_controlers.pen_delete_Page);
+/* GET delete pen page */
+router.get('/delete',secured, pen_controlers.pen_delete_Page);
 module.exports = router;
